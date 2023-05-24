02b535ddb9547c37c1da36ca458b6d346e0b61b4
TLRKiliann <philogenie@protonmail.com>
2023-05-24 16:47:49 +0200

    new page created for articles

---
+++ my-app/src/lib/articles/articlePage.ts
@@ -0,0 +1,34 @@
+export const articles = [
+	{
+		"id": 1,
+		"article": "The koala feeds very selectively on the leaves of certain \n"+ 
+		"eucalyptus trees. Generally solitary, individuals move within a home \n"+
+		"range of more than a dozen trees, one of which is favoured over the others.\n"+
+		"If koalas become too numerous in a restricted area, they defoliate preferred\n"+ 
+		"food trees and, unable to subsist on even closely related species, decline \n"+
+		"rapidly. To aid in digesting as much as 1.3 kg (3 pounds) of leaves daily, \n"+
+		"the koala has an intestinal pouch (cecum) about 2 metres (7 feet) long, where \n"+
+		"symbiotic bacteria degrade the tannins and other toxic and complex substances \n"+
+		"abundant in eucalyptus. This diet is relatively poor in nutrients and provides \n"+
+		"the koala little spare energy, so the animal spends long hours simply sitting or \n"+
+		"sleeping in tree forks, exposed to the elements but insulated by thick fur. \n"+
+		"Although placid most of the time, koalas produce loud, hollow grunts."
+	},
+	{
+		"id": 2,
+		"article": "Baseball is still our country's pastime, but these days, watching \n"+
+		"America's national sport is a whole new ballgame — and not just because of the \n"+
+		"rule changes in the 2023 MLB season. With games split among cable, streaming and \n"+
+		"regional sports networks, plus local restrictions and blackouts added into the mix, \n"+
+		"keeping up with your favorite team can be hit or miss unless you’re willing to shell \n"+
+		"out for a pricey cable package (and even then, you might get hit with blackouts).\n"+
+		"But don’t worry, we’re stepping up to the plate. Here’s every MLB game you can stream \n"+
+		"this week without paying for cable, your local RSN or MLB Network."
+	},
+	{
+		"id": 3,
+		"article": "The news !!! Monstre ligne Monstre ligne Monstre ligne\n"+
+		"Monstre ligne Monstre ligne Monstre ligne Monstre ligne Monstre ligne"
+	},
+
+];
