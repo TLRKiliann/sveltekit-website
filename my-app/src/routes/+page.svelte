60dbab2ab2ee365c76472c250325d93e75cf328e
TLRKiliann <philogenie@protonmail.com>
2023-05-24 10:22:12 +0200

    bg color around img improved

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="main--div">
-  <h1 class="class--h2">
+  <h1 class="class--h1">
     {tastyCookie 
       ? `Welcome ${tastyCookie} to DigiCode !`
       : `Welcome to DigiCode !`
@@ -27,25 +27,20 @@
     background: linear-gradient(30deg, #f8f8ff, #dee8f0, #dee8f0);
     z-index: -1;
   }
-  .class--h2 {
+  .class--h1 {
     padding: 0px 20px;
     padding-top: 90px;
     margin: auto;
     font-size: 2.8rem;
-    color: #dee8f0;
+    color: #3f414d;
     text-shadow:
-    0px 0px 1px #fff,
-    2px 2px 1px orange,
-    3px 3px 1px #ffba00,
-    4px 4px 1px #ffba00,
-    5px 5px 1px #999,
-    10px 10px 3px #707070;
+    3px 3px 3px slategrey;
   }
   .div--img {
     display: flex;
     width: 80%;
     margin: 5% auto;
-    background: #333;
+    background: linear-gradient(30deg, slategrey, #333, #333);
     border-radius: 20px;
     box-shadow: 0px 0px 20px #000;
   }
