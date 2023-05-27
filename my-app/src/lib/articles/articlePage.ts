1d6d89e69d611d7938f83d6a5a6af93b7b87fcff
TLRKiliann <philogenie@protonmail.com>
2023-05-27 11:50:16 +0200

    more articles added

--- my-app/src/lib/articles/articlePage.ts
+++ my-app/src/lib/articles/articlePage.ts
@@ -17,11 +17,31 @@ export const articles: Articles = [
 		"abundant in eucalyptus. This diet is relatively poor in nutrients and provides \n"+
 		"the koala little spare energy, so the animal spends long hours simply sitting or \n"+
 		"sleeping in tree forks, exposed to the elements but insulated by thick fur. \n"+
+		"Although placid most of the time, koalas produce loud, hollow grunts.",
+		"secondarticle": "The koala feeds very selectively on the leaves of certain \n"+ 
+		"eucalyptus trees. Generally solitary, individuals move within a home \n"+
+		"range of more than a dozen trees, one of which is favoured over the others.\n"+
+		"If koalas become too numerous in a restricted area, they defoliate preferred\n"+ 
+		"food trees and, unable to subsist on even closely related species, decline \n"+
+		"rapidly. To aid in digesting as much as 1.3 kg (3 pounds) of leaves daily, \n"+
+		"the koala has an intestinal pouch (cecum) about 2 metres (7 feet) long, where \n"+
+		"symbiotic bacteria degrade the tannins and other toxic and complex substances \n"+
+		"abundant in eucalyptus. This diet is relatively poor in nutrients and provides \n"+
+		"the koala little spare energy, so the animal spends long hours simply sitting or \n"+
+		"sleeping in tree forks, exposed to the elements but insulated by thick fur. \n"+
 		"Although placid most of the time, koalas produce loud, hollow grunts."
 	},
 	{
 		"id": 2,
 		"article": "Baseball is still our country's pastime, but these days, watching \n"+
+		"America's national sport is a whole new ballgame — and not just because of the \n"+
+		"rule changes in the 2023 MLB season. With games split among cable, streaming and \n"+
+		"regional sports networks, plus local restrictions and blackouts added into the mix, \n"+
+		"keeping up with your favorite team can be hit or miss unless you’re willing to shell \n"+
+		"out for a pricey cable package (and even then, you might get hit with blackouts).\n"+
+		"But don’t worry, we’re stepping up to the plate. Here’s every MLB game you can stream \n"+
+		"this week without paying for cable, your local RSN or MLB Network.",
+		"secondarticle": "Baseball is still our country's pastime, but these days, watching \n"+
 		"America's national sport is a whole new ballgame — and not just because of the \n"+
 		"rule changes in the 2023 MLB season. With games split among cable, streaming and \n"+
 		"regional sports networks, plus local restrictions and blackouts added into the mix, \n"+
@@ -46,6 +66,18 @@ export const articles: Articles = [
 		"applications breaking because of patch changes, lack of personnel or time to install them, \n"+
 		"or other reasons.\n"+
 		"The attack began at 07:44 UTC on 12 May 2017 and was halted a few hours later at 15:03 \n"+
+		"UTC by the registration of a kill switch discovered by Marcus Hutchins. The kill switch \n"+
+		"prevented already infected computers from being encrypted or further spreading WannaCry.[6]\n"+ 
+		"The attack was estimated to have affected more than 300,000 computers[7] across 150 \n"+
+		"countries,[7] with total damages ranging from hundreds of millions to billions of dollars. \n"+
+		"At the time, security experts believed from preliminary evaluation of the worm that the \n"+
+		"attack originated from North Korea or agencies working for the country. This was confirmed \n"+
+		"in December 2017, when the United States and United Kingdom formally asserted that North \n"+
+		"Korea was behind the attack.[8]\n"+
+		"A new variant of WannaCry forced Taiwan Semiconductor Manufacturing Company (TSMC) to \n"+
+		"temporarily shut down several of its chip-fabrication factories in August 2018. The virus \n"+
+		"spread onto 10,000 machines in TSMC's most advanced facilities.[9]",
+		"secondarticle": "The attack began at 07:44 UTC on 12 May 2017 and was halted a few hours later at 15:03 \n"+
 		"UTC by the registration of a kill switch discovered by Marcus Hutchins. The kill switch \n"+
 		"prevented already infected computers from being encrypted or further spreading WannaCry.[6]\n"+ 
 		"The attack was estimated to have affected more than 300,000 computers[7] across 150 \n"+
