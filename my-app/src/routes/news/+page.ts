ded70f4514da0b6b1196901e2d49608b106e4194
TLRKiliann <philogenie@protonmail.com>
2023-06-01 11:47:50 +0200

    some modifications for external api calls (with api folder)

--- my-app/src/routes/news/+page.ts
+++ my-app/src/routes/news/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from "./$types";
 
 export const load = (async (loadEvent) => {
 	const { fetch } = loadEvent;
-	const response = await fetch('http://localhost:4000/news');
+	const response = await fetch('/api/news');
 	const news = await response.json();
 	return { news };
 }) satisfies PageLoad;
