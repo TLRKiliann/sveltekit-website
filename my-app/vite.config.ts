de252981865f8ab4d525cf4e08fdab2aaca1c9cc
TLRKiliann <philogenie@protonmail.com>
2023-06-05 11:16:35 +0200

    config vitest

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [
+    svelte({ hot: !process.env.VITEST }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
